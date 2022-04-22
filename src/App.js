87072211fdfd29e8e49be23c5d07dab364559fc8
Omar-macbook <omar987@hotmail.co.uk>
2022-04-22 10:55:28 +0100

    moved showResults into top level for ease. will include a new Movie component which we will call after collecting the second batch of data.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './Header';
-import ResultsPanel from './ResultsPanel';
 
 function App() {
 	const [searchInput, setSearchInput] = useState('');
@@ -10,22 +9,38 @@ function App() {
 
 	function handleSearch(event) {
 		event.preventDefault();
-		setSearchInput(event.target.value);
+		console.log(event.target.value);
+
+		setSearchInput(event?.target?.value);
 		fetchInitialSearchResults();
 	}
 
 	const fetchInitialSearchResults = async () => {
-		const res = await fetch(
-			`https://www.omdbapi.com/?apikey=3f3c26a6&s=${searchInput}&type=movie&page=1`
-		);
-		const data = await res.json();
-		setSearchResults(data.Search);
+		if (searchInput !== undefined) {
+			const res = await fetch(
+				`https://www.omdbapi.com/?apikey=3f3c26a6&s=${searchInput}&type=movie&page=1`
+			);
+			const data = await res.json();
+			setSearchResults(data.Search);
+		}
 	};
 
+	const showResults = searchResults?.map((result) => (
+		<div className="movie-card">{JSON.stringify(result)}</div>
+	));
+	// const showResults = searchResults?.map((result) => {
+	// 	console.log(result);
+	// 	return searchResults.map((result) => (
+	// 		<div className="movie-card">{JSON.stringify(result)}</div>
+	// 	));
+	// });
 	return (
 		<div className="App">
 			<Header handleSearch={handleSearch} />
-			<ResultsPanel results={searchResults} />
+			<div className="movies-div">
+				{searchResults?.length >= 1 && showResults}
+			</div>
+			;
 		</div>
 	);
 }
